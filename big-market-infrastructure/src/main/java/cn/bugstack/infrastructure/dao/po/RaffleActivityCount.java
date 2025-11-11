6227414a93775d290792e607bc7be54ce4b255ca
qifengleqifengle <2472846459@qq.com>
2025-11-11 20:43:24 +0800

    第12节：用户参与抽奖活动库表设计

---
+++ big-market-infrastructure/src/main/java/cn/bugstack/infrastructure/dao/po/RaffleActivityCount.java
@@ -0,0 +1,44 @@
+package cn.bugstack.infrastructure.dao.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class RaffleActivityCount {
+
+    /**
+     * 自增ID
+     */
+    private Long id;
+
+    /**
+     * 活动次数编号
+     */
+    private Long activityCountId;
+
+    /**
+     * 总次数
+     */
+    private Integer totalCount;
+
+    /**
+     * 日次数
+     */
+    private Integer dayCount;
+
+    /**
+     * 月次数
+     */
+    private Integer monthCount;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+}
---
+++ big-market-infrastructure/src/main/java/cn/bugstack/infrastructure/dao/po/RaffleActivityOrder.java
@@ -0,0 +1,59 @@
+package cn.bugstack.infrastructure.dao.po;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class RaffleActivityOrder {
+
+    /**
+     * 自增ID
+     */
+    private Long id;
+
+    /**
+     * 用户ID
+     */
+    private String userId;
+
+    /**
+     * 活动ID
+     */
+    private Long activityId;
+
+    /**
+     * 活动名称
+     */
+    private String activityName;
+
+    /**
+     * 抽奖策略ID
+     */
+    private Long strategyId;
+
+    /**
+     * 订单ID
+     */
+    private String orderId;
+
+    /**
+     * 下单时间
+     */
+    private Date orderTime;
+
+    /**
+     * 订单状态（not_used、used、expire）
+     */
+    private String state;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+}
